9a67f7a52fcf4686ca5fca7bb72ba38f493e6b56
付青云 <1366773912@qq.com>
2022-03-24 00:27:05 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//    char a, b;
+//    scanf("%c", &a);
+//    char c;
+//    c = getchar();
+//    scanf("%c", &b);
+//    a = a - ' ';
+//    b = b - ' ';
+//    printf("%c\n%c", a, b);
+//    return 0;
+//}
+
+//int search(int* nums, int numsLen, int target) 
+//{
+//    // write code here
+//    int left = 0;
+//    int right = numsLen - 1;
+//    int mid = 0;
+//    while (right >= left)
+//    {
+//        mid = (left + right) / 2;
+//        if (*(nums + mid) > target)
+//        {
+//            right--;
+//        }
+//        else if (*(nums + mid) < target)
+//        {
+//            left++;
+//        }
+//        else
+//        {
+//            return mid;
+//        }
+//    }
+//    if (right < left)
+//    {
+//        return -1;
+//    }
+//}
+//
+//int main()
+//{
+//    int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//    int a = search(arr, 9, 4);
+//    printf("%d\n", a);
+//    return 0;
+//}
+
